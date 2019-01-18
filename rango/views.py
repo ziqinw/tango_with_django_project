eba753584228d9515de012b75237cfed70bc8d2e
Zi Qin Wong <2318997w@student.gla.ac.uk>
2019-01-18 09:55:57 +0000

    chapter 4.2

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie"}
+    return render(request, 'rango/index.html', context=context_dict)
