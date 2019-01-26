2f2ba338b83f3976f2bfe543bbfca61659e65509
Zi Qin Wong <2318997w@student.gla.ac.uk>
2019-01-26 13:06:05 +0000

    chapter 7 add category

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,6 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^about/', views.about, name = 'about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
-    url(r'^page/(?P<page_name_slug>[\w\-]+)/$', views.show_page, name='show_page'),
     ]
--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -36,3 +37,14 @@ def show_page(request, page_name_slug):
         context_dict['pages'] = None
         context_dict['category'] = None
     return render(request, 'rango/page.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html',{'form':form})
