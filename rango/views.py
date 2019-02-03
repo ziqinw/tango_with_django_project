2acd36b71d53216f55422fe12539ececde81654b
Zi Qin Wong <2318997w@student.gla.ac.uk>
2019-02-03 16:22:04 +0000

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -12,8 +12,9 @@ def index(request):
     return render(request, "rango/index.html", context_dict)
 
 def about(request):
-    context_dict = {"message": "Rango says here is the about page"}
-    return render(request, "rango/about.html", context=context_dict)
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html',{})
 
 def show_category(request, category_name_slug):
     context_dict = {}
